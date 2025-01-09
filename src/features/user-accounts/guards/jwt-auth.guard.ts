b520174ed7a297782907d109b6379aa44f2cf9a7
seeyous00n <borovikov.learn@gmail.com>
2025-01-09 08:51:01 +0300

    feat: add jwt-auth guard

---
+++ src/features/user-accounts/guards/jwt-auth.guard.ts
@@ -0,0 +1,40 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { SETTINGS } from '../../../core/settings';
+
+const BEARER = 'Bearer';
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+  async canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: SETTINGS.JWT_ACCESS_SECRET,
+      });
+
+      request['userId'] = payload.userId;
+    } catch {
+      throw new UnauthorizedException();
+    }
+
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === BEARER ? token : undefined;
+  }
+}
