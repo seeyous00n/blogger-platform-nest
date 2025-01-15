f6c9d0d6ef67172a0f5219731e8359f9ca404ff5
seeyous00n <borovikov.learn@gmail.com>
2025-01-15 16:03:47 +0300

    refactor

--- src/core/pipes/object-id-validation-pipe.service.ts
+++ src/core/pipes/object-id-validation-pipe.service.ts
@@ -7,7 +7,6 @@ import {
 } from '@nestjs/common';
 import { isValidObjectId } from 'mongoose';
 
-//TODO Дима рекомендует не делать эти валидации в пайпах, лучше это все делать в сервисах
 @Injectable()
 export class ObjectIdValidationPipe implements PipeTransform {
   transform(value: string, metadata: ArgumentMetadata) {
