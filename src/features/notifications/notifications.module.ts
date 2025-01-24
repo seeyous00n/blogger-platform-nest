16b1eca7824259203ce55083675fed5be24102f9
seeyous00n <borovikov.learn@gmail.com>
2025-01-25 01:34:46 +0300

    refactor

--- src/features/notifications/notifications.module.ts
+++ src/features/notifications/notifications.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
 import { MailerModule } from '@nestjs-modules/mailer';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     MailerModule.forRootAsync({
-      // imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         transport: {
           host: configService.get<string>('SMTP_HOST'),
